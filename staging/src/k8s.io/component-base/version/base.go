fe1728c47c29e6de13e34097b78140d8f4b09b38
rafaelbreno[commit] <rafael_breno@outlook.com>
2026-01-27 16:20:39 +0000

    v1.33.7-k3s2

--- staging/src/k8s.io/component-base/version/base.go
+++ staging/src/k8s.io/component-base/version/base.go
@@ -10,8 +10,8 @@ const (
 var (
 	gitMajor = "1"
 	gitMinor = "33"
-	gitVersion   = "v1.33.3-k3s1"
-	gitCommit    = "5a12bfb95082aea01e67312c91878aff93944471"
+	gitVersion   = "v1.33.7-k3s2"
+	gitCommit    = "c3ac10a179bc64b5474ac6349cadb7f718ec8f66"
 	gitTreeState = "clean"
-	buildDate = "2025-07-16T14:38:43Z"
+	buildDate = "2026-01-27T16:19:21Z"
 )
